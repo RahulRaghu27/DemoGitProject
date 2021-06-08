cf1008906cbcee8ec2160479452f8d6277307a1c
RahulRaghu27 <74955830+RahulRaghu27@users.noreply.github.com>
2021-06-08 23:37:25 +0530

    Second commit

--- src/com/domain/company/SampleGit.java
+++ src/com/domain/company/SampleGit.java
@@ -3,7 +3,7 @@ package com.domain.company;
 public class SampleGit {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome To Git");
+		System.out.println("Welcome To Git!!!");
 
 	}
 
